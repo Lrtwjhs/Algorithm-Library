2d1f20c4e900d9f6bcb2a34015a64a1adc8e0bff
Lrtwjhs <lrtwjhs@gmail.com>
2017-08-22 18:47:50 +0800

    upload

---
+++ Java/26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public int removeDuplicates(int[] A) {
+	    if (A.length == 0) return 0;
+	    int len = 1;
+	    for(int i = 1; i < A.length; i++) {
+	        if(A[i] != A[i - 1]) {
+	             for(int j = i - 1; j > len - 1; j--)
+	                 A[j] = A[i];
+	             len++;
+	        }
+	    }
+	    return len;
+	}
+}
---
+++ Java/27. Remove Element.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        if(nums.length == 0) return 0;
+
+        int len = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+        	if(nums[i] != val) nums[len++] = nums[i];
+        }
+        return len;
+    }
+}

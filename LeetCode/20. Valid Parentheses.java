656364312887e732b7f447c1792429fd50392907
Lrtwjhs <lrtwjhs@gmail.com>
2018-06-25 16:06:19 +0800

    add solution

--- LeetCode/20. Valid Parentheses.java
+++ LeetCode/20. Valid Parentheses.java
@@ -6,5 +6,36 @@
 class Solution {
     public boolean isValid(String s) {
         char[] stack = new char[s.length()];
+        int top = -1;
+
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case ')':
+                    if (top >= 0 && stack[top] == '(') {
+                        top--;
+                    } else {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (top >= 0 && stack[top] == '{') {
+                        top--;
+                    } else {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (top >= 0 && stack[top] == '[') {
+                        top--;
+                    } else {
+                        return false;
+                    }
+                    break;
+                default:
+                    stack[++top] = c;
+                    break;
+            }
+        }
+        return top < 0;
     }
 }
---
+++ LeetCode/28. Implement strStr().java
@@ -0,0 +1,21 @@
+
+/**
+ * Created by lrtwjhs on 2018/6/25.
+ */
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int l1 = haystack.length(), l2 = needle.length();
+        for (int i = 0, j; i + l2 - 1 < l1; i++) {
+            for (j = 0; j < l2; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            if (j >= l2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
